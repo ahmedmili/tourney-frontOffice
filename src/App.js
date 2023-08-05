ecef917bfe88ed82206ea723cb19143f078d5488
a.mili <ahmed.mili.webify@gmail.com>
2023-08-05 11:23:52 +0100

    [Ref] convert app to functional component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
- 
+
 // import './App.css';
- 
+
 import React from 'react';
 import {
   BrowserRouter as Router,
@@ -19,50 +19,26 @@ import SearchPage from './pages/search-page';
 import PartnersDetailsPage from './pages/searvice-details-page';
 import ProfilePage from './pages/Profile';
 import AddPartnairePage from './pages/add-partner';
- 
-export default class App extends React.Component {
 
 
-  constructor(props) {
-    super(props);
-    this.state = {
+function App() {
+  return (
+    <div >
 
-    }
-  }
+      <Router>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+          <Route path={'/search'} component={SearchPage} exact />
+          <Route path={'/addPartenaire'} component={AddPartnairePage} exact />
+          <Route path={'/partners/:id'} component={PartnersDetailsPage} exact />
+          <Route path={'/profile'} component={ProfilePage} exact />
+          <Route path="/auth" component={AuthPage} exact />
+          <Route path="/create-account" component={CreateAccountPage} exact />
+        </Switch>
+      </Router>
+    </div>
 
-  render() {
-    return (
-      <div >
-
-        <Router>
-          <Switch>
-            <Route path="/" component={ HomePage } exact/>
-            <Route path={ '/search' } component={ SearchPage } exact />
-            <Route path={ '/addPartenaire' } component={ AddPartnairePage } exact />
-            <Route path={ '/partners/:id' } component={ PartnersDetailsPage } exact />
-            
-
-            <Route path={ '/profile' } component={ ProfilePage } exact />
-            
-
-            
-
-
-
-            <Route path="/auth" component={ AuthPage } exact/>
-
-            <Route path="/create-account" component={ CreateAccountPage } exact/>
-            
-            
-   
-          </Switch>
-        </Router>
-      </div>
-
-    );
-  }
-
-
+  );
 }
 
 
